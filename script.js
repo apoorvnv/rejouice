eb92a7d69cf9db900e51fd4b0ba0e084a2e5d0c1
Apoorv <apoorvnv@gmail.com>
2024-05-27 23:31:49 +0530

    updating js for gsap.to

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var page1Content = document.querySelector("#page1-content")
 var cursor = document.querySelector("#cursor")
 
 page1Content.addEventListener("mousemove", function(dets){
-    cursor.style.left = dets.x+"px"
-    cursor.style.top = dets.y+"px"
-    
+    gsap.to("#cursor",{
+        x:dets.x,
+        y:dets.y
+    })
 })
